8c1242b296482e356ac8920b360044222320bc8f
nikhilvkn <nikhilnarayanan2007@gmail.com>
2020-06-12 07:43:21 +0530

    confiiguring multiple path

--- server.py
+++ server.py
@@ -4,6 +4,12 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def main():
     return render_template('index.html')
+
+
+@app.route('/<string:url_path>')
+def path(url_path):
+    return render_template(url_path)
