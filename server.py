2dfa4b0f6c936961a26683e2537d60a47206e8a9
nikhilvkn <nikhilnarayanan2007@gmail.com>
2020-06-15 09:37:10 +0530

    correcting code html css

--- server.py
+++ server.py
@@ -9,7 +9,10 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-
 @app.route('/<string:url_path>')
 def path(url_path):
     return render_template(url_path)
+
+@app.route('/count-service')
+def count_service():
+	return render_template('count-service.html', data='This is a test page')
