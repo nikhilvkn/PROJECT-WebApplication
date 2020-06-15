72c3967de5eefd9f028660b9b8b50cf37de06080
nikhilvkn <nikhilnarayanan2007@gmail.com>
2020-06-15 09:40:48 +0530

    adding service-count route

--- server.py
+++ server.py
@@ -15,4 +15,25 @@ def path(url_path):
 
 @app.route('/count-service')
 def count_service():
-	return render_template('count-service.html', data='This is a test page')
+   if request.method == 'POST':
+      datacenter = request.form['Datacenter']
+      environment = request.form['Environment']
+      result = {}
+      service_list = []
+
+      data = InceptionTools(datacenter)
+      work_fulldata = data.dc_data()
+
+      for elements in work_fulldata['dynconfigMonitoringServerUrls']:
+         for values in elements['url']:
+            if elements['environment'] == environment:
+               service_list.append(values['container'])
+      count = Counter(service_list)
+      counter = 0
+      for key, value in count.items():
+         if value < 3:
+            counter += 1
+            result[key] = str(value)
+      return render_template('service-dict-result.html', data=result)
+      if counter == 0:
+         return render_template('server-exception.html', data='All services have 3 or more instances')
